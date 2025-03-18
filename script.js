1ef8d9a8c3a3a21d0c6b844a08821fa6886c539e
Jacob Menlove <jacobmenlove1@gmail.com>
2025-03-18 16:20:47 -0400

    added DOM elements javascript

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ let paperCount = 0;
 let scissorsCount = 0;
 let gameIsActive = false;
 
+const startButton = document.getElementById('startButton');
+const winsElement = document.getElementById('wins');
+const tiesElement = document.getElementById('ties');
+const lossesElement = document.getElementById('losses');
+const rockCountElement = document.getElementById('rockCount');
+const paperCountElement = document.getElementById('paperCount');
+const scissorsCountElement = document.getElementById('scissorsCount');
